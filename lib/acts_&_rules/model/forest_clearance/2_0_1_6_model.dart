17aca72e2bc66e344fd691d289389f7179b020c4
Saquib <saquib.parwez@in.ey.com>
2023-09-11 14:56:38 +0530

    Parivesh latest commit

---
+++ lib/acts_&_rules/model/forest_clearance/2_0_1_6_model.dart
@@ -0,0 +1,50 @@
+class Model2016{
+  String?name;
+  String?url;
+  Model2016({this.name,this.url});
+}
+List<Model2016>Model2016List=[
+  Model2016(
+    name: " Guidelines dated 30/11/2016: Guidelines for diversion of forest land for non-forest purpose under the Forest (Conservation) Act 1980- Submission of proposals to obtain approval for diversion of entire forest land located within a mining lease.",
+    url: "https://forestsclearance.nic.in/writereaddata/public_display/schemes/1852883352%24merged.pdf",
+  ), Model2016(
+    name: " Guidelines dated 17/11/2016: Guidelines for filling online form for seeking permission for grant of forest land on lease under section 2(iii) of Forest Conservation Act, 1980.",
+    url: "https://forestsclearance.nic.in/writereaddata/public_display/schemes/365912526%248%2031%202016.pdf",
+  ), Model2016(
+    name: " Guidelines dated 17/11/2016: Guidelines regarding prior approval of Central Government under the Forest (Conservation) Act, 1980 for all original documents of the all proposal with Ink Signature and stamp with relevant officer - reg.",
+    url: "https://forestsclearance.nic.in/writereaddata/public_display/schemes/2089458444%24Guidelines.pdf",
+  ), Model2016(
+    name: " Guidelines dated 08/11/2016: Diversion of forest land  for non-forestry purposes under Forest (Conservation) Act, 1980 - Guidelines for construction of community toilet for the benifit of the people-reg.",
+    url: "https://forestsclearance.nic.in/writereaddata/public_display/schemes/1563590967%24guidelines.pdf",
+  ), Model2016(
+    name: " Guidelines dated 24/10/2016: Guidelines for diversion of forest for land non-forest purposes under the Forest (Conservation) Act,1980- Guidelines for laying Transmission lines through forest area- reg.",
+    url: "https://forestsclearance.nic.in/writereaddata/public_display/schemes/1350975281%24Guidelines.pdf",
+  ), Model2016(
+    name: " Guidelines dated 30/09/2016: Guidelines regarding prior approval of Central Government under the Forest (Conservation) Act, 1980 (FC Act) for areas falling in the mining leases which were or are recorded as ‘forest’ in the Government record on or after the day the FC Act came into force, but while processing and or approving the proposals under the FC Act for use of forest land falling in such mining leases for mining purposes, such areas were treated as ‘non-forest – reg.",
+    url: "https://forestsclearance.nic.in/writereaddata/public_display/schemes/1456752499%24Guideline.pdf",
+  ), Model2016(
+    name: " Guidelines dated 29/09/2016: Guidelines for felling permission of trees in Eco-Sensitive Zone of Protected Areas- reg.",
+    url: "https://forestsclearance.nic.in/writereaddata/public_display/schemes/1744182729%24Guidelines.pdf",
+  ), Model2016(
+    name: " Guidelines dated 30/08/2016: Guidelines regarding prior approval of Central Government under the Forest (Conservation) Act, 1980 (FC Act) for areas falling in the mining leases which were or are recorded as 'forest' in the Government record on or after the day the FC Act came into force, but while processing and or approving the proposals under the FC Act for use of forest land falling in such mining leases for mining purposes, such areas were treated as 'non-forest — reg.",
+    url: "https://forestsclearance.nic.in/writereaddata/public_display/schemes/2119420234%24%208%2078%201996.pdf",
+  ), Model2016(
+    name: " Guidelines dated 20/06/2016: Guidelines for divresion of forest land for non-forest purpose under the Forest Conservation Act (1980) - Submission of proposals to obtain approval for diversion of entire forest land located within a mining lease.",
+    url: "https://forestsclearance.nic.in/writereaddata/public_display/schemes/1897386016%2411%20247%202016%20FC.pdf",
+  ), Model2016(
+    name: " Guidelines dated 31/03/2016: Guidelines for diversion of forest land for non-forest purposes under the forest (Conservation) Act, 1980- Submission of proposals to obtain approval for diversion of entire forest land located within a mining lease",
+    url: "https://forestsclearance.nic.in/writereaddata/public_display/schemes/2008980001%2411%2085%202016%20.pdf",
+  ), Model2016(
+    name: " Guidelines dated 16/03/2016: Guidelines for diversion of forest land for non-forestry purposes under the Forest (Conservation) Act, 1980- Guidelines for collection of Net Present Value (NPV).",
+    url: " https://forestsclearance.nic.in/writereaddata/public_display/schemes/1863447235%24Guidelines.pdf",
+  ), Model2016(
+    name: " Guidelines dated 09/03/2016: Guidelines regarding prior approval of the Central Government under the Forest Conservation (Act), 1980 for areas falling in the mining leases which were or are recorded as forest in the Government record on or after the day the FC came into force, but while processing and or approving the proposals under the FC Act for use of forest land falling in such mining leases for mining purposes such areas were treated as 'non-forest'-reg.",
+    url: "https://forestsclearance.nic.in/writereaddata/public_display/schemes/1230946319%248-78-96.pdf",
+  ), Model2016(
+    name: " Guidelines dated 03/03/2016: Guidelines under the Forest (Conservation) Act, 1980- Clarification on drilling of shot holes for seismic surveys-reg.",
+    url: "https://forestsclearance.nic.in/writereaddata/public_display/schemes/1923626151%24Guidelines.pdf",
+  ), Model2016(
+    name: " Guidelines dated 25/02/2016: General approval under section-2 of the Forest (Conservation) Act, 1980 for diversion of forest land for creation of critical public utility infrastructure by Government Departments involving not more than 5.00 hectares of forest land in each case in Left Wing Extremism (LWE) affected districts-reg.",
+    url: "https://forestsclearance.nic.in/writereaddata/public_display/schemes/1678519572%24Guidelines.pdf",
+  ),
+];
